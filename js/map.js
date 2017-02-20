b6662caa0c03040d5c58465bd61b0d7051b8bf8c
smontel <montelsylvain@gmail.com>
2017-02-20 16:46:38 +0100

    remove the satelite button on the map

--- js/map.js
+++ js/map.js
@@ -10,6 +10,7 @@ function myMap() {
     var mapProp = {
         center: new google.maps.LatLng(30, 0),
         zoom: 3,
+        disableDefaultUI: true
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
