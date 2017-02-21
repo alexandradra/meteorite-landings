a6c7182c4e9d1ebe9e349f145462a3462708899a
smontel <montelsylvain@gmail.com>
2017-02-21 17:33:50 +0100

    range slider added

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,15 @@ $("#openmenu").click(function(){
   $("#googleMap").toggleClass("open");
   $("#closemenu").toggleClass("open");
 });
+
+var handlesSlider = document.getElementById('slider-handles');
+
+$("#slider-years").ionRangeSlider({
+    type: "double",
+    grid: true,
+    min: 861,
+    max: 2013,
+    from: 861,
+    to: 1000,
+    prefix: "Year "
+});
